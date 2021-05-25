edae508cd216ec9d0cec4b2aabf354cde1896e2b
Joga Gunadharma <joga.sementara@.com>
2021-05-25 14:22:07 +0700

    adding testing viewmodel HomeActivityViewModelTest.kt

--- app/src/androidTest/java/com/amaeschiffer/moviedb/ui/HomeActivityTest.kt
+++ app/src/androidTest/java/com/amaeschiffer/moviedb/ui/HomeActivityTest.kt
@@ -4,12 +4,14 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.pressBack
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.amaeschiffer.moviedb.R
+import com.amaeschiffer.moviedb.data.TMDBData
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -17,13 +19,21 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4ClassRunner::class)
 class HomeActivityTest {
 
+    private val movies = TMDBData().getMovieJson()
+    private val tvShow = TMDBData().getTvShowJson()
+
     @Before
     fun setup(){
         ActivityScenario.launch(SplashScreen::class.java)
     }
 
     @Test
-    fun assertGetMovieFragment(){
+    fun checkHomeActivity(){
+        onView(withId(R.id.homeActivity)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun performClickedItemRecyclerView(){
         onView(withId(R.id.action_movies)).perform(click()).check(matches(isDisplayed()))
         onView(withId(R.id.rvMovies)).check(matches(isDisplayed()))
         onView(withId(R.id.rvMovies)).perform(
@@ -33,10 +43,7 @@ class HomeActivityTest {
             )
         )
         onView(withId(R.id.activityDetail)).check(matches(isDisplayed()))
-    }
-
-    @Test
-    fun assertGetTvFragment(){
+        onView(withId(R.id.activityDetail)).perform(pressBack())
         onView(withId(R.id.action_tvshow)).perform(click()).check(matches(isDisplayed()))
         onView(withId(R.id.rvTvShow)).check(matches(isDisplayed()))
         onView(withId(R.id.rvTvShow)).perform(
@@ -48,5 +55,18 @@ class HomeActivityTest {
         onView(withId(R.id.activityDetail)).check(matches(isDisplayed()))
     }
 
+    @Test
+    fun performScrollingRecyclerView(){
+        onView(withId(R.id.action_movies)).perform(click()).check(matches(isDisplayed()))
+        onView(withId(R.id.rvMovies)).check(matches(isDisplayed()))
+        onView(withId(R.id.rvMovies)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(movies.results.size)
+        )
 
+        onView(withId(R.id.action_tvshow)).perform(click()).check(matches(isDisplayed()))
+        onView(withId(R.id.rvTvShow)).check(matches(isDisplayed()))
+        onView(withId(R.id.rvTvShow)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(tvShow.results.size)
+        )
+    }
 }
---
+++ app/src/test/java/com/amaeschiffer/moviedb/viewmodel/HomeActivityViewModelTest.kt
@@ -0,0 +1,40 @@
+package com.amaeschiffer.moviedb.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class HomeActivityViewModelTest {
+
+    @Rule
+    @JvmField
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var homeActivityViewModel: HomeActivityViewModel
+
+    @Before
+    fun setUp(){
+        homeActivityViewModel = HomeActivityViewModel()
+    }
+
+    @Test
+    fun getMovies(){
+        homeActivityViewModel.loadDataMovie()
+        val movies = homeActivityViewModel.getMovies()
+        assertNotNull(movies)
+        assertEquals(20, movies?.value?.results?.size)
+    }
+
+    @Test
+    fun gettvShow(){
+        homeActivityViewModel.loadDataTv()
+        val tvShow = homeActivityViewModel.getTvShow()
+        assertNotNull(tvShow)
+        assertEquals(20, tvShow?.value?.results?.size)
+    }
+}
